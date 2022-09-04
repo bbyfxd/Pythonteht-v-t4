280424f3020b09c2ba18b23051985c33818b772f
Fadak <thisisfadak@gmail.com>
2022-09-04 19:25:57 +0300

    commit1

---
+++ teht_4.2.py
@@ -0,0 +1,14 @@
+inchvalue = int(input("Enter the value in inch: "))
+cmvalue = 2.54 * inchvalue
+
+while True:
+    inchvalue = int(input("Enter the value in inch: "))
+
+    if inchvalue > 0:
+        print('{}″ = {}cm'.format(inchvalue, cmvalue))
+    elif inchvalue == 0:
+        print('{}″ = {}cm'.format(inchvalue, cmvalue))
+
+
+    else:
+        print("End Program.")
